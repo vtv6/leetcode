9aac7fd712d53488e492cc8531ed36374b83d988
Vu <tuanvundvn@gmail.com>
2022-06-24 16:26:13 +0700

    medium: rotate image

---
+++ 48.go
@@ -0,0 +1,18 @@
+package main
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := i; j < n-i-1; j++ {
+			if i == j && i*2+1 == n {
+				continue
+			}
+
+			tp := matrix[i][j]
+			matrix[i][j] = matrix[n-j-1][i]
+			matrix[n-j-1][i] = matrix[n-i-1][n-j-1]
+			matrix[n-i-1][n-j-1] = matrix[j][n-i-1]
+			matrix[j][n-i-1] = tp
+		}
+	}
+}
